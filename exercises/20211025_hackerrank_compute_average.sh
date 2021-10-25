660637f9c01f8ff1d1fe5dfe4ef58500d771f962
Jong Shin <jshin.m@gmail.com>
2021-10-25 05:44:52 -0400

    add for loop solution

--- exercises/20211025_hackerrank_compute_average.sh
+++ exercises/20211025_hackerrank_compute_average.sh
@@ -3,14 +3,28 @@
 tmp=0;
 cnt=0;
 
-read num #skip first number (total number of input)
+read total #skip first number (total number of input)
 read num #first number
 
-while [ -n "$num" ]
+#while solution
+# while [ -n "$num" ]
+# do
+# cnt=$[cnt+1]
+# tmp=$[tmp+num]
+# read num
+# done
+
+#for loop solution
+#notice that there's no such thing as {1..$total}
+for i in $(seq 1 1 $total)
 do
 cnt=$[cnt+1]
 tmp=$[tmp+num]
 read num
 done
 
-echo "scale=3; $tmp/$cnt" | bc -l
+out=$(echo "$tmp/$cnt" | bc -l)
+
+if [[ $out == "0" ]]; then echo 0.000
+else printf %.3f $out
+fi
