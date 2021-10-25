1bf0cbffe81708ccd96c49d7a24a80014f78dd15
Jong Shin <jshin.m@gmail.com>
2021-10-25 03:01:47 -0400

    double bracket in numeric conditionals

--- exercises/20211024_hackerrank_comparing_numbers.sh
+++ exercises/20211024_hackerrank_comparing_numbers.sh
@@ -13,4 +13,30 @@ read y;
 if [ $x -lt $y ]; then echo "X is less than Y" #there must be semicolons always
 elif [ $x -gt $y ]; then echo "X is greater than Y"
 elif [ $x -eq $y ]; then echo "X is equal to Y"
+fi #fi must be in a separate line
+
+# Given three integers (x, y, and z) representing the three sides of a triangle, identify whether the triangle is scalene, isosceles, or equilateral.
+
+# If all three sides are equal, output EQUILATERAL.
+# Otherwise, if any two sides are equal, output ISOSCELES.
+# Otherwise, output SCALENE.
+# Input Format
+
+# Three integers, each on a new line.
+
+# Constraints
+# 1 <= val <= 100
+
+# The sum of any two sides will be greater than the third.
+
+# Output Format
+# One word: either "SCALENE" or "EQUILATERAL" or "ISOSCELES" (quotation marks excluded).
+
+read x;
+read y;
+read z;
+
+if [[ x -eq y && y -eq z ]]; then echo "EQUILATERAL"
+elif [[ x -eq y || x -eq z || y -eq z ]]; then echo "ISOSCELES"
+else echo "SCALENE"
 fi
