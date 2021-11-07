5bebe64bdc76d768a1c68d3d25ffd872b662b1b2
Jong Shin <jshin.m@gmail.com>
2021-11-06 22:57:49 -0400

    tr command single occurrence

--- exercises/20211106_hackerrank_text_processing.sh
+++ exercises/20211106_hackerrank_text_processing.sh
@@ -15,3 +15,18 @@
 # int i=[int]5.8
 # [23 + 5]*2
 tr '()' '[]'
+
+# In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+# In a given fragment of text, delete all the lowercase characters a-z.
+
+tr -d 'a-z'
+
+# In a given fragment of text, replace all sequences of multiple spaces with just one space.
+
+# Input Format
+# A block of ASCII text.
+
+# Output Format
+# Replace all sequences of multiple spaces with just one space.
+
+tr -s ' ' # replaces repeating character (in this case ' ') with a single occurrence of the character
